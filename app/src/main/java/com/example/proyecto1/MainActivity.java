2eb407a19a2e4868996e1b8c6e403f86047cfa96
Omar Méndez <mastercool507@hotmail.com>
2022-10-09 18:03:07 -0500

    Archivos de proyecto
    Proyecto final android

---
+++ app/src/main/java/com/example/proyecto1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.proyecto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+TextView textview;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
